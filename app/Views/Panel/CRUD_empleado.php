6c44fdc4b530edd6bcd0a79e7923af9e54c4b0a9
SoporteRassini <elchatgptplus1@gmail.com>
2026-01-27 15:36:39 -0600

    Updated information on the new version

--- app/Views/Panel/CRUD_empleado.php
+++ app/Views/Panel/CRUD_empleado.php
@@ -243,7 +243,7 @@
 
   <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0.1
+      <b>Version</b> 1.1.2
     </div>
     <strong>Copyright &copy; 2025 <a href="https://www.rassini.com/frenos/">Rassini Frenos</a>.</strong>
     Todos los derechos reservados.
